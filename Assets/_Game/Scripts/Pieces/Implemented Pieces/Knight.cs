0b0442b23860e9139d98e5254812fc46340950f9
Jasfiq Rahman <jasfiqrahman15@gmail.com>
2022-03-31 14:27:43 +0530

    Implemented Knight Logic

--- Assets/_Game/Scripts/Pieces/Implemented Pieces/Knight.cs
+++ Assets/_Game/Scripts/Pieces/Implemented Pieces/Knight.cs
@@ -6,11 +6,60 @@ public class Knight : ChessPiece
 {
     public override void GenerateValidMoves()
     {
+        _validMoves.Clear();
 
+        List<Vector2Int> candidateIndices = new List<Vector2Int>(8);
+        
+        Vector2Int increment = new Vector2Int(2, 1);
+        for (int i = 0; i < 4; i++)
+        {
+            Vector2Int move = _currentIndex + increment;
+            candidateIndices.Add(move);
+
+            increment.y *= -1;
+            if (i % 2 != 0)
+                increment.x *= -1;
+        }
+
+        increment = new Vector2Int(1, 2);
+        for (int i = 4; i < 8; i++)
+        {
+            Vector2Int move = _currentIndex + increment;
+            candidateIndices.Add(move);
+
+            increment.x *= -1;
+            if (i % 2 != 0)
+                increment.y *= -1;
+        }
+
+        foreach (Vector2Int indice in candidateIndices)
+        {
+            if (indice.x >= 0 && indice.x < 8 &&
+                indice.y >= 0 && indice.y < 8)
+            {
+                _validMoves.Add(new Vector2Int(indice.y, indice.x));
+            }
+        }
     }
 
     public override void GeneratePossibleMoves()
     {
+        _possibleMoves.Clear();
 
+        foreach (Vector2Int validMove in _validMoves)
+        {
+            ChessPiece piece = BoardManager.Instance.TileSet[validMove.y, validMove.x].HeldPiece;
+            if (piece)
+            {
+                if (piece.PieceData.PlayerType != _pieceData.PlayerType)
+                {
+                    _possibleMoves.Add(validMove);
+                }
+            }
+            else
+            {
+                _possibleMoves.Add(validMove);
+            }
+        }
     }
 }
